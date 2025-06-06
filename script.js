6c93fb2e3aa5ca80c6665903a066d1832790087c
Aman3007 <amanchauhan30072002@gmail.com>
2025-06-06 07:38:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
+
+ $(document).ready(function() {
 const link = "https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=df32153f9396f4db06ec6f38417d49cc";
-fetch(link).then((data) => {
+$("#refresh").on("click",()=>{
+ fetch(link).then((data) => {
     return data.json()
 }).then((newdata) => {
     const temp = Math.round(newdata.main.temp - 273.15);
     $("#location").html(newdata.name)
     $("#weather").html(newdata.weather[0].description)
-    $("#temp").prepend(temp)
+    document.getElementById("temp").innerHTML=temp;
     $("#icon").attr("src", "https://openweathermap.org/img/w/" + newdata.weather[0].icon + ".png")
 }).catch((error
 ) => {
     console.log(error)
 })
+});
+$("#refresh").click();
+ })
 
 
-//https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=df32153f9396f4db06ec6f38417d49cc
